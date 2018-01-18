62dda60681bbea3916060c0efa5b0172ee6baaf9
Jouryjc <494208562@qq.com>
2018-01-18 19:05:42 +0800

    键盘功能初步完成
    1、完成了键盘样式；
    2、基础的按键功能已经完成；
    
    TODOS：
    1、加入一些配置功能：
    a、允许加入正则匹配金额格式；
    b、自定义键盘按键高度；
    c、可自定义键盘“退出开启”效果；
    d、发布到npm；
    e、“支付”文案可自定义；
    
    技术栈：
    sass + vue + webpack + es6

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,182 @@
 <template>
-    <div id="app">
-      <div class="container">
-        <div class="rows">
-          <div class="rows-item">1</div>
-          <div class="rows-item">2</div>
-          <div class="rows-item">3</div>
-          <div class="rows-item"><</div>
+    <div id="app" @touchstart="typeIn" @contextmenu.prevent.stop :class="['animated' , { 'fadeInUp': keyboardShow , 'fadeOutDown': !keyboardShow}]">
+        
+        <div class="container">
+            <div class="item-group">
+                <div class="item">1</div>
+                <div class="item">2</div>
+                <div class="item">3</div>
+            </div>
+            <div class="item-group">
+                <div class="item">4</div>
+                <div class="item">5</div>
+                <div class="item">6</div>
+            </div>
+            <div class="item-group">
+                <div class="item">7</div>
+                <div class="item">8</div>
+                <div class="item">9</div>
+            </div>
+            <div class="item-group">
+                <div class="item hideKeyboard"></div>
+                <div class="item">0</div>
+                <div class="item">.</div>
+            </div>
         </div>
-        <div class="rows">
-          <div class="rows-item">4</div>
-          <div class="rows-item">5</div>
-          <div class="rows-item">6</div>
-          <div class="rows-item"></div>
+
+        <div class="sidebar">
+            <div class="sidebar-item delCurInput"></div>
+            <div class="sidebar-item">支付</div>
         </div>
-        <div class="rows">
-          <div class="rows-item">7</div>
-          <div class="rows-item">8</div>
-          <div class="rows-item">9</div>
-          <div class="rows-item"></div>
-        </div>
-        <div class="rows">
-          <div class="rows-item"></div>
-          <div class="rows-item">0</div>
-          <div class="rows-item">.</div>
-          <div class="rows-item"></div>
-        </div>
-      </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    data () {
+        return {
+            result: '',
+            keyboardShow: true
+        }
+    },
+    methods: {
+        typeIn(event) {
+            let self = this,
+                $tar = event.target || event.srcElment,
+                curInput = !!$tar.innerText ? $tar.innerText : $tar.classList[1],
+                str = self.result
+
+            if (curInput == "支付") {
+                console.log("get result then pay")
+                return;
+            }
+
+            if (curInput == "delCurInput") {
+                self.result = self.result.slice(0 , -1)
+                return;
+            }
+
+            if (curInput == "hideKeyboard" && self.keyboardShow) {
+                self.keyboardShow = false
+            }
+
+            if (curInput == "." && str.includes('.')) {
+                return;
+            }
+
+            if (str == '0' && curInput != '.') {
+                return;
+            }
+
+            if (str.slice(str.indexOf('.')).length == 3) {
+                return;
+            }
+
+            let _result = str + curInput
+
+            if (_result.length <= (_result.includes('.') ? 9 : 6)) {
+                self.result = _result
+            }
+
+        }
     }
-  }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    font-size: 20px;
+    position: fixed;
+    bottom: 0;
+
+    $borderStyle: solid thin #E3E3E3;
+
+    .container {
+        display: flex;
+        width: 75%;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+
+        .item-group {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+
+            .hideKeyboard {
+                background: url("./assets/keyboard.png");
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: 30px;
+            }
+            
+            .item {
+                width: 33.33%;
+                height: 60px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-right: $borderStyle;
+                border-bottom: $borderStyle;
+
+                &:active {
+                    background-color: #06BE04;
+                }
+            }
+
+            &:nth-child(1) {
+                border-top: $borderStyle;
+            }
+        }
+    }
+
+    .sidebar {
+        width: 25%;
+        float: right;
+        display: flex;
+        flex-direction: column;
+        border-top: $borderStyle;
+
+        .sidebar-item {
+            width: 100%;
+            height: 60px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-bottom: $borderStyle;
+
+            &:nth-child(1) {
+                background: url("./assets/delete.png");
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: 25px;
+            }
+            
+            &:nth-child(2) {
+                color: #ffffff;
+                background-color: #06BE04;
+                flex-grow: 2;
+            }
+
+            img {
+                width: 25px;
+            }
+
+            &:active {
+                background-color: #06BE04;
+            }
+        }
+    }
 }
-.container {
-  display: flex;
-  display: -webkit-flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  
-}
+
+
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,20 @@ module.exports = {
   },
   module: {
     rules: [
+        {
+            test: /(\.sass|\.scss)$/,
+            use: [
+                'sass-loader'
+            ]
+        },
       {
         test: /\.css$/,
         use: [
           'vue-style-loader',
           'css-loader'
         ],
-      },      {
+      }, 
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
@@ -33,12 +40,19 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/
       },
-      {
+      /* {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
         }
+      }, */
+      {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'url-loader',
+          options: {
+              limit: 10000
+          }
       }
     ]
   },
